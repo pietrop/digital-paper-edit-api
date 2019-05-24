da8e023c0cc5def3f8d330cd9d92e48a0aa8750c
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-05-24 22:19:43 +0100

    Refactor projects end point

--- routes/projects.js
+++ routes/projects.js
@@ -4,9 +4,8 @@
 
 // Dummy data to mock the server
 const sampleProjects = require('../sample-data/projects.sample.json');
-const sampleTranscripts = require('../sample-data/transcripts.sample.json');
-const samplePaperEdits = require('../sample-data/paper-edits.sample.json');
-
+const cuid = require('cuid');
+// TODO: add date created, and date updated to records ?
 /**
  * Projects
  */
@@ -16,19 +15,20 @@ module.exports = (app) => {
     // TODO: save to db
     console.log('req', req.body);
     // Just  a mock for testing purposes, when connecting to DB, ids are unique and immutable
-    const lasProjectPositionIndex = sampleProjects.projects.length === 0 ? 0 : sampleProjects.projects.length - 1;
-    const lastProjectId = sampleProjects.projects.length === 0 ? 0 : sampleProjects.projects[lasProjectPositionIndex].id;
-    const newProjectId = lastProjectId + 1;
-
-    sampleProjects.projects.push({
+    // const lasProjectPositionIndex = sampleProjects.projects.length === 0 ? 0 : sampleProjects.projects.length - 1;
+    // const lastProjectId = sampleProjects.projects.length === 0 ? 0 : sampleProjects.projects[lasProjectPositionIndex].id;
+    // const newProjectId = lastProjectId + 1;
+    const project = {
       title: req.body.title,
       description: req.body.description,
-      id: newProjectId
-    });
+      id: cuid(),
+      created: Date()
+    }
+    sampleProjects.projects.push(project);
 
     // TODO: send project ID?
     console.log('projects', 'post', '/api/projects ');
-    res.status(201).json({ status:'ok', projectId: newProjectId });
+    res.status(201).json({ status:'ok', project: project });
 
   });
 
@@ -37,13 +37,12 @@ module.exports = (app) => {
     // TODO: db
     console.log('projects', 'get', '/api/projects');
     res.status(200).json(sampleProjects);
-
   });
 
   // show - get individual project
   // https://expressjs.com/en/api.html#req
   app.get('/api/projects/:projectId', (req, res) => {
-    const projectId = parseInt(req.params.projectId);
+    const projectId = req.params.projectId;
     // TODO: db
     // tmpProject.project = sampleProjects.projects[parseInt(req.params.projectId)];
     const tmpProject = sampleProjects.projects.filter((p) => {
@@ -56,26 +55,37 @@ module.exports = (app) => {
 
   // edit
   app.put('/api/projects/:projectId', (req, res) => {
+    const projectId = req.params.projectId;
+    const newProject = {
+      "id": projectId,
+        "title": req.body.title,
+        "description":req.body.description
+    }
+    const projectIndex = sampleProjects.projects.findIndex(item => item.id === projectId);
+    sampleProjects.projects[projectId] = newProject;
+
     // TODO: db
     // to access data
     // req.body.title
     // req.body.id || req.params.projectId
     // req.body.description
     console.log('projects', 'put', `/api/projects/${ req.params.projectId }/edit`, req.body);
-    res.status(200).json({ status: 'ok' });
+    res.status(200).json({ status: 'ok' , project: newProject});
   });
 
   // delete
   app.delete('/api/projects/:projectId/', function (req, res) {
-    const projectId = parseInt(req.params.projectId);
-
-    projectToDelete = sampleProjects.projects[projectId];
-    // Tmp to testing UI
-    delete sampleProjects.projects[projectId];
-
+    const projectId = req.params.projectId;
+    console.log(projectId);
+    const projectToDelete = sampleProjects.projects.filter((p) => {
+      return p.id === projectId;
+    })[0];
+    const projectToDeleteId = projectToDelete.id;
     sampleProjects.projects = sampleProjects.projects.filter((p) => {
-      return p.id !== projectId;
+      return p.id !== projectToDeleteId;
     });
+     // Tmp to testing UI
+     delete projectToDelete;
     // TODO: db
     // TODO: filter sampleProjects for those that don't match ?
     console.log('projects', 'deleted', `/api/projects/${ projectToDelete }`);
