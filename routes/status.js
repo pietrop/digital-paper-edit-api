a87844a95bdd5688c7bd43c0edf7d691ead0dbcb
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-05-24 12:34:07 +0100

    linting

--- routes/status.js
+++ routes/status.js
@@ -1,10 +1,9 @@
-// health check on /status to 200 is needed for cosmos AWS deployment 
+// health check on /status to 200 is needed for cosmos AWS deployment
 
 // TODO: add some server error handling that if it's having issues
 // it does not return 200
 module.exports = (app) => {
-    app.get('/status', (req, res) => {
-        res.sendStatus(200);
-    });
-}
-
+  app.get('/status', (req, res) => {
+    res.sendStatus(200);
+  });
+};
