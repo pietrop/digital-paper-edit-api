14722817561080bdf91c51ff31017d79007dff10
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-05-28 08:37:28 +0100

    refactor labels

--- routes/labels.js
+++ routes/labels.js
@@ -1,7 +1,5 @@
 // Dummy data to mock the server
-const sampleProjects = require('../sample-data/projects.sample.json');
-const sampleTranscripts = require('../sample-data/transcripts.sample.json');
-const samplePaperEdits = require('../sample-data/paper-edits.sample.json');
+const cuid = require('cuid');
 const sampleLabels = require('../sample-data/labels.sample.json');
 /**
  * annotations
@@ -9,21 +7,22 @@ const sampleLabels = require('../sample-data/labels.sample.json');
 module.exports = (app) => {
   // New
   app.post('/api/projects/:projectId/labels', (req, res) => {
+    const newLabel = req.body;
+    newLabel.id = cuid()
+    // console.log('POSTPOST - req.body ',req);
     // TODO: save to db
     const projectId = req.params.projectId;
     console.log('labels', 'post', `/api/projects/${ projectId }/labels`);
     // TODO: send project ID?
-    res.status(201).json({ status:'ok' });
-
+    sampleLabels.labels.push(newLabel)
+    res.status(201).json({ status:'ok', labels: sampleLabels.labels });
   });
   //index - list labels
   app.get('/api/projects/:projectId/labels', (req, res) => {
     // TODO: read from db
     const projectId = req.params.projectId;
     console.log('labels', 'get', `/api/projects/${ projectId }/labels`);
-    // TODO: send project ID?
-    const response = { labels: sampleLabels };
-    res.status(200).json(response);
+    res.status(200).json({ status:'ok' ,labels: sampleLabels.labels });
   });
 
   // show
@@ -32,33 +31,39 @@ module.exports = (app) => {
     // TODO: read from db
     const projectId = req.params.projectId;
     const labelId = req.params.labelId;
+    const annotationIndex =  sampleLabels.labels.findIndex(item => item.id === labelId);
+    const tmpLabel = sampleLabels.labels[annotationIndex] 
     console.log('labels', 'get', `/api/projects/${ projectId }/labels/${ labelId }`);
     // TODO: send project ID?
-    const response = { labels: sampleLabels[2] };
-    res.status(200).json(response);
-  });
-
-  // edit - index
-  // TODO: decide if always editing labels in batch or doing /labels/:labeldId
-  // to edit one at a time for consistency
-  app.put('/api/projects/:projectId/labels', (req, res) => {
-    const projectId = req.params.projectId;
-    console.log('labels', 'put', `/api/projects/${ projectId }/labels`);
-    res.status(200).json({ status:'ok' });
+ 
+    res.status(200).json({ label: tmpLabel });
   });
 
   // edit - one
   app.put('/api/projects/:projectId/labels/:labelId', (req, res) => {
     const projectId = req.params.projectId;
+    const labelId = req.params.labelId;
+    const updatedLabel = req.body
+    console.log('updatedLabel', updatedLabel);
+    // updated label
+    const labelIndex =  sampleLabels.labels.findIndex(item => item.id === labelId);
+    sampleLabels.labels[labelIndex] = updatedLabel;
+
     console.log('labels', 'put', `/api/projects/${ projectId }/labels/${ labelId }`);
-    res.status(200).json({ status:'ok' });
+    res.status(200).json({ status:'ok', labels: sampleLabels.labels });
   });
 
   // delete
   app.delete('/api/projects/:projectId/labels/:labelId', function (req, res) {
     const projectId = req.params.projectId;
     const labelId = req.params.labelId;
+    // Delete label
+    delete sampleLabels.labels[labelId];
+    sampleLabels.labels = sampleLabels.labels.filter((item) => {
+      return item.id !== labelId;
+    });
+
     console.log('labels', 'delete', `/api/projects/${ projectId }/labels/${ labelId }`);
-    res.status(200).json({ status:'ok' });
+    res.status(200).json({ status:'ok', labels: sampleLabels.labels  });
   });
 };
