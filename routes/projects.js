16ba1303993a03acd223ef44ef5f58fb4ce46e8b
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-12 18:25:21 +0100

    adde db wrapper to projects route

--- routes/projects.js
+++ routes/projects.js
@@ -1,47 +1,56 @@
 const cuid = require('cuid');
 const logger = require('../lib/logger.js');
 
-const data = require('../sample-data/projects.sample.json');
+const db = require('../dbWrapper/index.js');
+// const data = require('../sample-data/projects.sample.json');
 
 module.exports = (app) => {
   app.post('/api/projects', (req, res) => {
-    const project = {
+    const projectData = {
       title: req.body.title,
       description: req.body.description,
-      id: cuid(),
+      // ID created by the dbWrapper
       created: Date(),
     };
-
-    data.projects.push(project);
-
-    logger.info(`POST: New project ${ project.id }`);
+    const project = db.create('projects', projectData);
+    // Temporary workaround.
+    // otherwise would need to change all client side ids to be _id instead of id
+    project.id = project._id;
+    logger.info(`POST: New project ${ project._id }`);
     res.status(201).json({ status: 'ok', project });
   });
 
   app.get('/api/projects', (req, res) => {
+    const data = {};
+    data.projects = db.getAll('projects')
+      // Temporary workaround.
+      .map((project) => {
+        project.id = project._id;
+
+        return project;
+      });
+    // TODO: consider adding data to logs?
     logger.info('GET: Projects');
     res.status(200).json(data);
   });
 
   app.get('/api/projects/:projectId', (req, res) => {
     const projectId = req.params.projectId;
-
-    const project = data.projects.find(p => p.id === projectId);
+    const project = db.get('projects', { _id: projectId });
+    // const project = data.projects.find(p => p._id === projectId);
     logger.info(`GET: Project id ${ req.params.projectId }`);
     res.status(200).json({ project });
   });
 
   app.put('/api/projects/:projectId', (req, res) => {
     const projectId = req.params.projectId;
-
     const newProject = {
       id: projectId,
       title: req.body.title,
       description: req.body.description,
     };
 
-    const projectIndex = data.projects.findIndex(item => item.id === projectId);
-    data.projects[projectIndex] = newProject;
+    db.update('projects', { _id: projectId }, newProject);
 
     logger.info(`PUT: Edit project id ${ req.params.projectId }`);
     res.status(200).json({ status: 'ok', project: newProject });
@@ -50,10 +59,9 @@ module.exports = (app) => {
   app.delete('/api/projects/:projectId/', (req, res) => {
     const projectId = req.params.projectId;
 
-    const projectToDelete = data.projects.find(p => p.id === projectId);
-    data.projects = data.projects.filter(p => p.id !== projectToDelete.id);
+    db.delete('projects', { _id: projectId });
 
     logger.info(`DELETE: Project id ${ projectId }`);
-    res.status(204).json({ status: 'ok' });
+    res.status(204).json({ status: 'ok', project: {} });
   });
 };
